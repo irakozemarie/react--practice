426d63b5698e4f7760da3e1b2d738489a0b1ee57
irakozemarie <irakoze.antonny@gmail.com>
2024-04-23 11:49:28 +0200

    new features

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,22 @@ import Home from "./Components/Home";
 import AboutUS from "./Components/AboutUS";
 import Contact from "./Components/Contact";
 import React from "react";
+import Navbar from "./Components/Navbar";
 function App() {
   return (
-    <Router>
-      <Link to="/Home">Home</Link>
-      <Link to="/About"> AboutUS</Link>
-      <Link to="/Contact"> Contact</Link>
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="/About" element={<AboutUS />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-    </Router>
+    <>
+      <Navbar />
+      <Router>
+        <Link to="/Home">Home</Link>
+        <Link to="/About"> AboutUS</Link>
+        <Link to="/Contact"> Contact</Link>
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<AboutUS />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 export default App;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,6 @@
+const Navbar = () => {
+  return (
+    <div className="bg-green-400 text-blue-500">Irakoze Chance</div>
+  )
+}
+export default Navbar
